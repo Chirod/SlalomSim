357517be6416e0ff1636e48fee93c665a00f6b9e
Christopher Wohlwend <christopher.wohlwend@gmail.com>
2023-09-11 11:09:09 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::mem::swap;
-use std::rc::Rc;
-use std::{ops::Add};
-use minifb::{MouseMode, Window, WindowOptions, ScaleMode, Scale};
-use raqote::{DrawTarget, SolidSource, Source, DrawOptions, PathBuilder, Point, Transform, StrokeStyle, Path};
-use font_kit::family_name::FamilyName;
-use font_kit::properties::Properties;
-use font_kit::source::SystemSource;
+use std::ops::Deref;
+use std::ops::Add;
+use std::thread::sleep;
+use std::time::Duration;
+use minifb::{Window, WindowOptions};
+use rand::Rng;
+use raqote::{DrawTarget, SolidSource, Source, DrawOptions, PathBuilder};
 
+#[derive(Debug)]
 struct CourseLineSeg (CoursePoint, CoursePoint);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct CoursePoint {
     downcourse: i32,
     sidecourse: i32
@@ -32,7 +32,7 @@ impl BuoySegs {
         let quarter_mark: CourseLineSeg;
         let half_mark: CourseLineSeg = CourseLineSeg(buoy_location, buoy_location + CoursePoint::create(4100, 0));
         let buoy_downcourse = buoy_location.downcourse;
-        let full_mark: CourseLineSeg; 
+        let full_mark: CourseLineSeg;
         if buoy_location.sidecourse > 0 {
             quarter_mark = CourseLineSeg(buoy_location, buoy_location + CoursePoint::create(0, 1000));
             full_mark = CourseLineSeg(CoursePoint::create(buoy_downcourse, 115), CoursePoint::create(buoy_downcourse + 4100, 115));
@@ -50,71 +50,25 @@ impl BuoySegs {
     }
 }
 
-#[derive(PartialEq)]
-enum SegOrientation {
-    Colinear,
-    Clockwise,
-    CouterClockwise,
-}
-
 impl CourseLineSeg {
-    fn on_segment( p: CoursePoint, q:  CoursePoint, r: CoursePoint) -> bool {
-        q.downcourse <= std::cmp::max(p.downcourse, r.downcourse) && 
-        q.downcourse >= std::cmp::min(p.downcourse, r.downcourse) &&
-        q.sidecourse <= std::cmp::max(p.sidecourse, r.sidecourse) &&
-        q.sidecourse >= std::cmp::min(p.sidecourse, r.sidecourse)
-    }
-    fn orientation(p: CoursePoint, q: CoursePoint, r: CoursePoint) -> SegOrientation{
-        match (q.sidecourse - p.sidecourse) * (r.downcourse - q.downcourse) - (q.downcourse - p.downcourse) * (r.sidecourse - q.sidecourse)
-        {
-            0 => SegOrientation::Colinear,
-            1..=i32::MAX => SegOrientation::Clockwise,
-            i32::MIN..=-1 => SegOrientation::CouterClockwise,
-        }
-    }
-
-    fn intersect(p1: CoursePoint, q1: CoursePoint, p2: CoursePoint, q2: CoursePoint) -> bool
-    {
-        // Find the four orientations needed for general and
-        // special cases
-        let o1: SegOrientation = Self::orientation(p1, q1, p2);
-        let o2: SegOrientation = Self::orientation(p1, q1, q2);
-        let o3: SegOrientation = Self::orientation(p2, q2, p1);
-        let o4: SegOrientation = Self::orientation(p2, q2, q1);
-    
-        // General case
-        if o1 != o2 && o3 != o4 {
-            return true;
-        }
-            
-    
-        // Special Cases
-        // p1, q1 and p2 are collinear and p2 lies on segment p1q1
-        if o1 == SegOrientation::Colinear && Self::on_segment(p1, p2, q1) {
-            return true;
-        } 
-    
-        // p1, q1 and q2 are collinear and q2 lies on segment p1q1
-        if o2 == SegOrientation::Colinear  && Self::on_segment(p1, q2, q1) {
-            return true;
-        }
-    
-        // p2, q2 and p1 are collinear and p1 lies on segment p2q2
-        if o3 == SegOrientation::Colinear && Self::on_segment(p2, p1, q2) {
-            return true;
-        }
-    
-        // p2, q2 and q1 are collinear and q1 lies on segment p2q2
-        if o4 == SegOrientation::Colinear && Self::on_segment(p2, q1, q2) {
-            return true;
-        }
-
-        return false 
-    }
-
-
     fn intersects(&self, other: &Self) -> bool {
-        Self::intersect(self.0, other.0, self.1, other.1)
+        // Self::intersect(self.0, other.0, self.1, other.1)
+
+		use line_intersection::LineInterval;
+		use geo::Line;
+
+		let segment = LineInterval::line_segment(Line {
+			start: (self.0.downcourse as f64, self.0.sidecourse as f64).into(),
+			end: (self.1.downcourse as f64, self.1.sidecourse as f64).into(),
+		});
+
+		let line = LineInterval::line(Line {
+			start: (other.0.downcourse as f64, other.0.sidecourse as f64).into(),
+			end: (other.1.downcourse as f64, other.1.sidecourse as f64).into(),
+		});
+
+		let intersection = segment.relate(&line).unique_intersection();
+		intersection.is_some()
     }
 }
 impl Add for CoursePoint {
@@ -161,27 +115,38 @@ impl SlalomCourse {
         viewscreen.draw_buoy(CoursePoint::create(0,-125));
         viewscreen.draw_buoy(CoursePoint::create(Self::END_GATE_DOWNCOURSE,125));
         viewscreen.draw_buoy(CoursePoint::create(Self::END_GATE_DOWNCOURSE,-125));
-
     }
 }
 
-#[derive(Clone)]
 struct SkierPath {
-    prev: Option<Rc<SkierPath>>,
+    prev: Option<Box<SkierPath>>,
     current: CoursePoint
+}
+
+impl SkierPath {
+	fn pop(&mut self) {
+		let temp = std::mem::take(&mut self.prev);
+		if let Some(mut t) = temp {
+			self.prev = std::mem::take(&mut t.prev);
+		}
+		else {
+			unreachable!();
+		}
+	}
+}
+
+impl Default for SkierPath {
+    fn default() -> Self {
+        Self { prev: Default::default(), current: CoursePoint { downcourse: 0, sidecourse: 0 } }
+    }
 }
 
 impl Drop for SkierPath {
     fn drop(&mut self) {
+		let mut pop_count = 0;
         while self.prev.is_some() {
-            let prev: Rc::<SkierPath> = Rc::clone(&self.prev.as_ref().unwrap());
-            if prev.prev.is_some() {
-                let mut prev_prev = Some(Rc::clone(&prev.prev.as_ref().unwrap()));
-                std::mem::swap(&mut self.prev, &mut prev_prev);
-            }
-            else {
-                break;
-            }
+			self.pop();
+			pop_count += 1;
         }
     }
 }
@@ -198,35 +163,51 @@ impl Skier {
         Self {downcourse_speed: 10, sidecourse_speed: 0, ski_path: SkierPath { prev: None, current: start_loc }, max_accel: 0.0}
     }
     fn next_tic(&mut self, acceleration: (i32, i32)) -> CoursePoint {
-        let (down_a, side_a) = acceleration;
+		let (down_a, side_a) = acceleration;
         let absolute_acceleration = f64::sqrt((down_a * down_a + side_a * side_a).into());
         if self.max_accel < absolute_acceleration {
             self.max_accel = absolute_acceleration;
         }
         self.downcourse_speed += down_a;
         self.sidecourse_speed += side_a;
-        let previous = self.ski_path.clone();
-        let current = previous.current + CoursePoint::create(self.downcourse_speed, self.sidecourse_speed); 
-        self.ski_path = SkierPath {
-            prev: Some(Rc::new(previous)),
+        let current = self.ski_path.current + CoursePoint::create(self.downcourse_speed, self.sidecourse_speed);
+		let previous = std::mem::take(&mut self.ski_path);
+		self.ski_path = SkierPath {
+            prev: Some(Box::new(previous)),
             current
         };
         self.ski_path.prev.as_ref().unwrap().current
     }
 }
 
+#[derive(Clone)]
 struct SkierAgent {
-    call_count: u32
+	call_count: usize,
+    moves: [(i8, i8); 4000],
 }
 
 impl SkierAgent {
+	fn revise_prev_accel(&mut self) {
+		self.call_count -= 1;
+		self.mutate_index(self.call_count);
+	}
     fn get_next_acceleration(&mut self) -> (i32, i32) {
-        self.call_count += 1;
-        println!("{}", self.call_count);
-        (0,0)
+        let (left, right) = self.moves[self.call_count];
+		self.call_count += 1;
+		(left as i32, right as i32)
     }
-    fn create() -> Self {
-        Self { call_count:0 }
+	fn mutate_index(&mut self, index: usize) {
+		let mut rng = rand::thread_rng();
+		self.moves[index] = (rng.gen::<i8>() % 10, rng.gen::<i8>() % 10);
+	}
+	fn mutate(&mut self)  {
+		self.mutate_index(rand::thread_rng().gen::<usize>() % 4000)
+	}
+}
+
+impl Default for SkierAgent {
+    fn default() -> Self {
+        Self { call_count:0, moves: [(0,0); 4000] }
     }
 }
 
@@ -234,6 +215,25 @@ struct ViewScreen {
     size: (usize, usize),
     dt: DrawTarget,
     window: Window,
+}
+
+#[derive(Debug)]
+struct SkiScore {
+	entry_gates: bool,
+	exit_gates: bool,
+	score: u16
+}
+
+impl SkiScore {
+	fn missed_entry() -> Self {
+		SkiScore { entry_gates: false, exit_gates: false, score: 0 }
+	}
+	fn no_exit( score: u16) -> Self {
+		SkiScore { entry_gates: true, exit_gates: false, score }
+	}
+	fn full_pass() -> Self {
+		SkiScore { entry_gates: false, exit_gates: false, score: 600 }
+	}
 }
 
 impl ViewScreen {
@@ -256,7 +256,7 @@ impl ViewScreen {
         self.window.is_open()
     }
     fn convert_course_point(loc:CoursePoint) -> (f32, f32) {
-        let mut floated = ((loc.sidecourse as f32) / 40.0, (loc.downcourse as f32) / 40.0);
+        let mut floated = ((loc.sidecourse as f32) / 20.0, (loc.downcourse as f32) / 20.0);
         floated.0 += 400.0;
         floated.1 = 700.0 - floated.1;
         floated
@@ -275,8 +275,18 @@ impl ViewScreen {
         let path = pb.finish();
         self.dt.fill(&path, &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0xff, 0)), &DrawOptions::new());
     }
-    fn draw_skier(&mut self, skier_location: Rc<SkierPath>) {
-        let mut next = skier_location;
+	fn draw_segment(&mut self, segment: &CourseLineSeg) {
+		let mut pb = PathBuilder::new();
+		let (px, py) = Self::convert_course_point(segment.0);
+		let (qx, qy) = Self::convert_course_point(segment.1);
+		let length = qx - px;
+		println!("{length}, {px} {qx}");
+		pb.rect(px, py, length, 1.0);
+		let path = pb.finish();
+		self.dt.fill(&path, &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0xff, 0)), &DrawOptions::new());
+	}
+	fn draw_skier(&mut self, skier_location: &SkierPath) {
+        let mut next: &SkierPath = skier_location;
         loop {
             let mut pb = PathBuilder::new();
             let (side, down) = Self::convert_course_point(next.current);
@@ -285,19 +295,21 @@ impl ViewScreen {
             self.dt.fill(&path, &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0, 0xff)), &DrawOptions::new());
             match &next.prev {
                 None => return,
-                Some(x) => next = Rc::clone(x),
+                Some(x) => {
+					next = x.deref();
+				}
             }
         }
     }
     fn window_loop(&mut self) {
         if self.is_open() {
             self.window.update_with_buffer(self.dt.get_data(), self.size.0, self.size.1).unwrap();
-            self.dt.clear(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0xff, 0xff));
+            self.dt.clear(SolidSource::from_unpremultiplied_argb(0xff, 0xEE, 0xEE, 0xff));
         }
     }
 }
 
-fn score_skier_agent(mut skier_agent: SkierAgent, course: &mut SlalomCourse, viewscreen: &mut ViewScreen) -> (u16, Skier) {
+fn score_skier_agent(mut skier_agent: SkierAgent, course: &mut SlalomCourse, viewscreen: &mut ViewScreen) -> (SkiScore, Skier) {
     let mut boat_location: i32 = -6000;
     let end_of_course = (41 * 5 + 27 * 2 + 55) * 100;
     // let mut boat_location: i32 = 22250;
@@ -311,17 +323,35 @@ fn score_skier_agent(mut skier_agent: SkierAgent, course: &mut SlalomCourse, vie
     let mut x = 0;
     while boat_location < end_of_course {
         boat_location += 10;
-        let previous = skier.next_tic(skier_agent.get_next_acceleration());
-        let distance: f64 = skier.ski_path.current.distance_from(&CoursePoint::create(boat_location, 0));
-        let fudge: f64 = distance - f64::from(rope_length);
-        if fudge.abs() > skier_reach {
-            break;
-        }
+		let mut previous;
+		loop {
+			let accel = skier_agent.get_next_acceleration();
+			previous = skier.next_tic(accel);
+			let distance: f64 = skier.ski_path.current.distance_from(&CoursePoint::create(boat_location, 0));
+			let fudge: f64 = distance - f64::from(rope_length);
+			if fudge.abs() > skier_reach {
+				skier_agent.revise_prev_accel();
+			}
+			else {
+				break;
+			}
+		}
         let segment = CourseLineSeg(previous, skier.ski_path.current);
         if !entry_gates {
+			if boat_location > 2700 {
+				break;
+			}
+			if(segment.0.downcourse < 0 && segment.1.downcourse > 0) {
+				println!("HAH, {segment:?} {:?}", course.entry_gate);
+			}
             if course.entry_gate.intersects(&segment) {
                 entry_gates = true;
+				println!("got_there");
             }
+			if(segment.0.downcourse < 0 && segment.1.downcourse > 0) {
+				println!("Oh!");
+				break;
+			}
         }
         if !exit_gates {
             if course.exit_gate.intersects(&segment) {
@@ -344,43 +374,57 @@ fn score_skier_agent(mut skier_agent: SkierAgent, course: &mut SlalomCourse, vie
                     buoys[x].2 = true;
                 }
             }
+			if (true, true, true) != buoys[x] && boat_location > ((41 * (x as i32 + 2) - 14) * 100) {
+				break;
+			}
         }
         x += 1;
         x %= 500;
         if x == 0 {
             course.draw_buoys(viewscreen);
             viewscreen.draw_boat(boat_location);
-            viewscreen.draw_skier(Rc::new(skier.ski_path.clone()));
+            viewscreen.draw_skier(&skier.ski_path);
             viewscreen.window_loop();
         }
     }
     if !entry_gates {
-        return (0, skier);
+        return (SkiScore::missed_entry(), skier);
     }
     for x in 0..5u16 {
         match buoys[x as usize] {
             (true, true, true) => continue,
-            (true, true, false) => return (x * 100 + 50, skier),
-            (true, false, _) => return (x * 100 + 25, skier),
-            (false, _, _) => return (x, skier),
+            (true, true, false) => return (SkiScore::no_exit(x * 100 + 50), skier),
+            (true, false, _) => return (SkiScore::no_exit(x * 100 + 25), skier),
+            (false, _, _) => return (SkiScore::no_exit(x), skier),
         }
     }
     if exit_gates {
-        return (600, skier);
+        return (SkiScore::full_pass(), skier);
     }
     else {
-        return (599, skier);
+        return (SkiScore::no_exit(600), skier);
     }
 }
 
+
+fn tests() {
+	let gates = CourseLineSeg(CoursePoint { downcourse: 0, sidecourse: 125 }, CoursePoint { downcourse: 0, sidecourse: -125 });
+	let skier = CourseLineSeg(CoursePoint { downcourse: -5, sidecourse: 0 }, CoursePoint { downcourse: 5, sidecourse: 0 });
+	assert!(gates.intersects(&skier));
+}
+
+
 fn main() {
+	tests();
     let mut viewscreen = ViewScreen::create();
     let mut course = SlalomCourse::create();
-    let (score, skier) = score_skier_agent(SkierAgent::create(), &mut course, &mut viewscreen);
-    println!("score: {score}");
-    while viewscreen.is_open() {
-        course.draw_buoys(&mut viewscreen);
-        viewscreen.draw_skier(Rc::new(skier.ski_path.clone()));
-        viewscreen.window_loop();
+    let (score, skier) = score_skier_agent(SkierAgent::default(), &mut course, &mut viewscreen);
+    println!("score: {score:?}");
+	course.draw_buoys(&mut viewscreen);
+    viewscreen.draw_skier(&skier.ski_path);
+	while viewscreen.is_open() {
+		sleep(Duration::from_millis(200));
+		viewscreen.window_loop();
+
     }
 }
